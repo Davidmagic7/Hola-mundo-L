2d2d69e473f9b76d6a1b84238618fb3b5814656b
David Torres <juandavidtorreaa@gmail.com>
2024-09-14 13:55:35 -0500

    se agrega el proyecto

---
+++ C++/hola_mundo.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "hola mundo" << std::endl;
+    return 0;
+}
---
+++ Dart/hola_mundo.dart
@@ -0,0 +1,3 @@
+void main() {
+  print("hola mundo");
+}
