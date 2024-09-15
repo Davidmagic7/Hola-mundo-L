30d7ec8f116d7225860012eae74d391eacc7b939
Davidtorres <juandavidtorreaa@gmail.com>
2024-09-15 16:00:47 -0500

    Actualización de archivos Go y Python: se añadiron nuesvas funciones

--- Go/hola_mundo.go
+++ Go/hola_mundo.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
+    
     fmt.Println("Hola Mundo")
+
+    unaLista := []string{"e", "a", "f", "c", "b", "d"}
+    sort.Strings(unaLista)
+    fmt.Println("Orden ascendente:", unaLista)
+
+    unaListaReves := []string{"e", "a", "f", "c", "b", "d"}
+    sort.Sort(sort.Reverse(sort.StringSlice(unaListaReves)))
+    fmt.Println("Orden descendente:", unaListaReves)
 }
--- Python/hola_mundo.py
+++ Python/hola_mundo.py
@@ -1,1 +1,13 @@
 print("hola mundo")
+
+una_lista= ["e", "a", "f", "c" , "b" , "d"]
+
+una_lista.sort()
+
+print(una_lista)
+
+una_lista_reves= ["e", "a", "f", "c" , "b" , "d"]
+
+una_lista_reves.sort(reverse=True)
+
+print(una_lista_reves)
